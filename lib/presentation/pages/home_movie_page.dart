66e287c04f1f6d9b95d8c443615bbd96331f8e76
afrinaldiGenzo <afrinaldi190401@gmail.com>
2024-09-04 12:33:16 +0700

    ADD : module about

--- lib/presentation/pages/home_movie_page.dart
+++ lib/presentation/pages/home_movie_page.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ditonton/common/constants.dart';
 import 'package:ditonton/domain/entities/movie.dart';
-import 'package:ditonton/presentation/pages/about_page.dart';
+import 'package:about/about.dart';
 import 'package:ditonton/presentation/pages/movie_detail_page.dart';
 import 'package:ditonton/presentation/pages/now_playing_series_page.dart';
 import 'package:ditonton/presentation/pages/popular_movies_page.dart';
